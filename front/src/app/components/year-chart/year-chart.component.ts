d43989d5392a7f4854474f4d11437ddfd7e64c43
qurppy <qurppy@gmail.com>
2019-04-04 12:20:00 +0200

    some changes

---
+++ front/src/app/components/year-chart/year-chart.component.ts
@@ -0,0 +1,70 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ChartDataSets, ChartOptions } from 'chart.js';
+import { Color, BaseChartDirective, Label } from 'ng2-charts';
+import * as pluginAnnotations from 'chartjs-plugin-annotation';
+
+
+@Component({
+  selector: 'app-year-chart',
+  templateUrl: './year-chart.component.html',
+  styleUrls: ['./year-chart.component.scss']
+})
+export class YearChartComponent implements OnInit {
+  public lineChartData: ChartDataSets[] = [
+    { data: [65, 59, 80, 81, 56, 55, 140], label: 'Wydatki' },
+    { data: [28, 48, 40, 19, 86, 27, 90], label: 'Przychody' },
+  ];
+  public lineChartLabels: Label[] = ['Styczen', 'Luty', 'Marzec', 'Kwiecien', 'Maj', 'Czerwiec', 'Lipiec'];
+  public lineChartOptions: (ChartOptions & { annotation: any }) = {
+    responsive: true,
+    scales: {
+      // We use this empty structure as a placeholder for dynamic theming.
+      xAxes: [{}],
+      yAxes: [
+        {
+          id: 'y-axis-0',
+          position: 'left',
+        }
+      ]
+    },
+    annotation: {
+    },
+  };
+  public lineChartColors: Color[] = [
+    { // grey
+      backgroundColor: 'rgba(148,159,177,0.2)',
+      borderColor: 'rgba(148,159,177,1)',
+      pointBackgroundColor: 'rgba(148,159,177,1)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgba(148,159,177,0.8)'
+    },
+    { // red
+      backgroundColor: 'rgba(255,0,0,0.3)',
+      borderColor: 'red',
+      pointBackgroundColor: 'rgba(148,159,177,1)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgba(148,159,177,0.8)'
+    }
+  ];
+  public lineChartLegend = true;
+  public lineChartType = 'line';
+  public lineChartPlugins = [pluginAnnotations];
+
+  @ViewChild(BaseChartDirective) chart: BaseChartDirective;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  // events
+  public chartClicked({ event, active }: { event: MouseEvent, active: {}[] }): void {
+    console.log(event, active);
+  }
+
+  public chartHovered({ event, active }: { event: MouseEvent, active: {}[] }): void {
+    console.log(event, active);
+  }
+}
--- front/src/app/pages/events/events.component.ts
+++ front/src/app/pages/events/events.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import {EventService} from 'src/app/service/event.service';
 
 @Component({
   selector: 'app-events',
   templateUrl: './events.component.html',
   styleUrls: ['./events.component.scss']
 })
-export class EventsComponent implements OnInit {
 
-  constructor() { }
+
+
+export class EventsComponent implements OnInit {
+  weekEvents: any;
+  monthEvents: any;
+
+  constructor(private eventService: EventService) { }
 
   ngOnInit() {
+    console.log(this.eventService.EXAMPLE_DATA)
+    this.weekEvents = this.eventService.EXAMPLE_DATA;
+    this.monthEvents = this.eventService.EXAMPLE_DATA;
   }
 
 }
