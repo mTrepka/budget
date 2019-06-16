49083e88ad8e902d1ce2ee904c7a96709f0013f8
qurppy <qurppy@gmail.com>
2019-06-16 13:56:09 +0200

    Add security based on JWT

---
+++ front/src/app/guards/auth.guard.ts
@@ -0,0 +1,43 @@
+import {Injectable} from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  CanActivateChild,
+  CanLoad,
+  Route,
+  Router,
+  RouterStateSnapshot,
+  UrlSegment,
+  UrlTree
+} from '@angular/router';
+import {Observable} from 'rxjs';
+import {AuthenticationService} from '../service/authentication.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate, CanActivateChild, CanLoad {
+
+  constructor(private router: Router, private authenticationService: AuthenticationService) { }
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    const currentUser = this.authenticationService.currentUserValue;
+    if (currentUser) {
+      return true;
+    }
+    this.router.navigate(['/login'],{ queryParams: {returnUrl: state.url}});
+    return false;
+  }
+  canActivateChild(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return true;
+  }
+  canLoad(
+    route: Route,
+    segments: UrlSegment[]): Observable<boolean> | Promise<boolean> | boolean {
+    return true;
+  }
+}
---
+++ front/src/app/service/authentication.service.ts
@@ -0,0 +1,46 @@
+import {Injectable} from '@angular/core';
+import {BehaviorSubject, Observable} from 'rxjs';
+import {HttpClient} from '@angular/common/http';
+import {map} from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticationService {
+  private currentUserSubject: BehaviorSubject<string>;
+  public currentUser: Observable<string>;
+
+  constructor(private http: HttpClient) {
+    this.currentUserSubject = new BehaviorSubject<string>(localStorage.getItem('currentUser'));
+    this.currentUser = this.currentUserSubject.asObservable();
+  }
+
+  public get currentUserValue(): string {
+    return this.currentUserSubject.getValue();
+  }
+
+  login(user: string, pass: string) {
+
+    const u = {
+      username: user,
+      password: pass
+    };
+
+    return this.http.post('http://localhost:8080/login', u, {observe: 'response'})
+      .pipe(map(resp => {
+        if (resp.headers.get('Authorization')) {
+          this.currentUserSubject.next(user);
+          localStorage.setItem('currentUser', user);
+          localStorage.setItem('token', resp.headers.get('Authorization'));
+        }
+        return resp;
+      }));
+  }
+
+  logout() {
+    localStorage.removeItem('currentUser');
+    localStorage.removeItem('token');
+    this.currentUserSubject.next(null);
+  }
+
+}
